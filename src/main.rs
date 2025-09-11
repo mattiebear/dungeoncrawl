7abb8d50646bcb27d01bcbae689d143ebedd23b3
Matthew Sells <matthew.sells@proton.me>
2025-09-11 16:11:59 -0500

    Add base map generation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,60 @@
-use bracket_lib::prelude::*;
+mod map;
+mod map_builder;
+mod player;
 
-struct State {}
+mod prelude {
+    pub use bracket_lib::prelude::*;
+
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
+}
+
+use crate::prelude::*;
+
+struct State {
+    map: Map,
+    player: Player,
+}
 
 impl State {
     fn new() -> Self {
-        Self {}
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, world!");
+
+        self.player.update(ctx, &self.map);
+
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
+        .with_resource_path("resources/")
+        .with_font("dungeonfont.png", 32, 32)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .build()?;
 
     main_loop(context, State::new())
