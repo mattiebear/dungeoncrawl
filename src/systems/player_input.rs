0bd8e0b44304d11db1f214239d9bfa83834b2661
Matthew Sells <matthew.sells@proton.me>
2025-09-17 11:33:12 -0500

    Upgrade to message system

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,15 +1,16 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -19,21 +20,18 @@ pub fn player_input(
             _ => Point::zero(),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            // This is kind of odd. Looks like you can also do:
-            // let mut players: Query<&mut Point> = Query::new();
-            // players = players.filter(component::<Player>());
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        });
 
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+        *turn_state = TurnState::PlayerTurn;
     }
 }
