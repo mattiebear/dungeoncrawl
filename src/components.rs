f07b12f542f4871fa8ca6cb4fbb14cd1cf0f2f49
Matthew Sells <matthew.sells@proton.me>
2025-10-07 16:16:22 -0500

    Spawn the amulet

--- src/components.rs
+++ src/components.rs
@@ -38,3 +38,9 @@ pub struct Health {
 
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
