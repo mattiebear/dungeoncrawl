e872ff687c0d6306ea715884b552ccf5492321e5
Matthew Sells <matthew.sells@proton.me>
2025-10-07 15:04:10 -0500

    Display game over screen

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     // First time this runs the player is the only attacker.
@@ -14,6 +15,12 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         .collect();
 
     victims.iter().for_each(|(intent, victim)| {
+        let is_player = ecs
+            .entry_ref(*victim)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
+
         if let Ok(health) = ecs
             .entry_mut(*victim) // This is how we query the health when all we have is the Entity
             .unwrap()
@@ -22,7 +29,7 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
             println!("Health before attack: {}", health.current);
             health.current -= 1;
 
-            if health.current <= 0 {
+            if health.current <= 0 && !is_player {
                 commands.remove(*victim);
             }
 
